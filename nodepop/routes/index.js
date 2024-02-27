3da21ac709e7675dfd666a52200c49471936937a
Saul Dominguez <saul.vau@gmail.com>
2024-02-27 21:10:57 +0100

    la vista ya esta casi pero faltaria la foto no olvidar las validaciones y el eslint

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -2,10 +2,23 @@ var express = require('express');
 var router = express.Router();
 var Anuncio = require('../models/Anuncio');
 
+router.use(express.static('public'));
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nodepop' });
+router.get('/', async function(req, res, next) {
+  try {
+    
+    const listaAnuncios = await Anuncio.find();
+    console.log(listaAnuncios);
+    res.render('index', {listaAnuncios, title: 'Nodepop' });
+
+  } catch (error) {
+    next(error);
+  }
+  
 
 });
+
+
+
 
 module.exports = router;
